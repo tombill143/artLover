f4cede558c1b4a565ee3734f74e10aa24faa9566
tombill143 <tombill143@gmail.com>
2021-12-07 16:32:51 +0100

    I think I have worked out uploading the project

---
+++ src/main/java/com/example/artlover/ArtLoverApplication.java
@@ -0,0 +1,13 @@
+package com.example.artlover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtLoverApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ArtLoverApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/artlover/ArtLoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artlover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtLoverApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
