2562e59d82f416021270b68c11f7dc8e961d2d3d
tombill143 <tombill143@gmail.com>
2021-12-07 18:32:13 +0100

    Completed my entities

--- src/main/java/com/example/artlover/Entity/artist1ShoppingCart.java
+++ src/main/java/com/example/artlover/Entity/artist1ShoppingCart.java
@@ -1,4 +1,36 @@
 package com.example.artlover.Entity;
 
 public class artist1ShoppingCart {
+
+  private int portraitId;
+  private int quantity;
+
+  public int getPortraitId() {
+    return portraitId;
+  }
+
+  public void setPortraitId(int portraitId) {
+    this.portraitId = portraitId;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
+
+  @Override
+  public String toString() {
+    return "artist1ShoppingCart{" +
+            "portraitId=" + portraitId +
+            ", quantity=" + quantity +
+            '}';
+  }
+
+
+
+
+
 }
