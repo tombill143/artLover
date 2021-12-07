b6a10d7d401ba163375d555da18ad5924882a0c1
Mahfuzur061 <mahf0017@stud.kea.dk>
2021-12-07 18:31:05 +0100

    entities updated

--- src/main/java/com/example/artlover/Entity/portrait.java
+++ src/main/java/com/example/artlover/Entity/portrait.java
@@ -1,4 +1,40 @@
 package com.example.artlover.Entity;
 
 public class portrait {
+    int dateOfCreation;
+    String nameOfArtist;
+    int id;
+    String portraitName;
+
+    public int getDateOfCreation() {
+        return dateOfCreation;
+    }
+
+    public void setDateOfCreation(int dateOfCreation) {
+        this.dateOfCreation = dateOfCreation;
+    }
+
+    public String getNameOfArtist() {
+        return nameOfArtist;
+    }
+
+    public void setNameOfArtist(String nameOfArtist) {
+        this.nameOfArtist = nameOfArtist;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPortraitName() {
+        return portraitName;
+    }
+
+    public void setPortraitName(String portraitName) {
+        this.portraitName = portraitName;
+    }
 }
