965e2e719dc5b0fe23fd2872d7d6b2c67e57c1ad
tombill143 <tombill143@gmail.com>
2021-12-16 18:20:13 +0100

    Just doing data entries for Artist1
    # Conflicts:
    #	src/main/java/com/example/artlover/Entity/portrait.java

--- src/main/java/com/example/artlover/Controller/Artist1Controller.java
+++ src/main/java/com/example/artlover/Controller/Artist1Controller.java
@@ -43,12 +43,22 @@ public class Artist1Controller{
     Artist1 artistThree = new Artist1(3, "22 Westfield rd", 25, "wpistons@yahoo.com", "Winston", "Piston", 65821567);
     Artist1 artistFour = new Artist1(4, "8 Carrill Grove", 60, "waferman@btinternet.com", "Jeff", "Wafer", 61225974);
     Artist1 artistFive = new Artist1(5, "14 Cromwell Avenue", 60, "sporky@hotmail.com", "Matilda", "Spork", 67816359);
-     Artist1 artistSix = new Artist1(6, "1 Tibb Street", 60, "bruceleeroy@outlook.com", "Jimmy", "Pigeon", 67816359);
-    artist1Service.getArtist1Repo().save(artist);
+    Artist1 artistSix = new Artist1(6, "1 Tibb Street", 21, "bruceleeroy@outlook.com", "Jimmy", "Pigeon", 60591823);
+    Artist1 artistSeven = new Artist1(7, "5 Buckfast Close", 44, "faxmachinemessage@gmail.com", "Jasper", "Pampers", 61557532);
+    Artist1 artistEight = new Artist1(8, "32 West Wallaby Street", 53, "iamasausage@yahoo.com", "Ron", "Waffle", 70541298);
+    Artist1 artistNine = new Artist1(9, "9 Treebeard Lane", 70, "putthekettleon@AOL.com", "John", "Caramel", 50941777);
+     Artist1 artistTen= new Artist1(9, "101 Tick Tock drive", 70, "honkeytonk@gmail.com", "Don", "Bonkers", 60265811);
+     artist1Service.getArtist1Repo().save(artist);
      artist1Service.getArtist1Repo().save(artistTwo);
      artist1Service.getArtist1Repo().save(artistThree);
      artist1Service.getArtist1Repo().save(artistFour);
      artist1Service.getArtist1Repo().save(artistFive);
+     artist1Service.getArtist1Repo().save(artistSix);
+     artist1Service.getArtist1Repo().save(artistSeven);
+     artist1Service.getArtist1Repo().save(artistEight);
+     artist1Service.getArtist1Repo().save(artistNine);
+     artist1Service.getArtist1Repo().save(artistTen);
+
      return "The Artists mentioned in the controller should be now uploaded to the database. Check the MYSQL database artLover to make sure changes have been made";
    }
 }
